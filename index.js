0e9f5616a0672bfbf52c3a9b85ffcd0a6f594d68
Isaac Brandyebrry <isaac@brandyberry.us>
2024-07-30 13:04:55 -0400

    Node MongoDB Example 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { MongoClient } = require('mongodb-legacy');
 const assert = require('assert');
+const dboper = require('./operations');
 
 const url = 'mongodb://localhost:27017/';
 const dbName = 'nucampsite';
@@ -8,30 +9,39 @@ MongoClient.connect(url, {}, (err, client) => {
   assert.strictEqual(err, undefined);
 
   console.log('Connected correctly to server');
-  const db = client.db(dbName)
+
+  const db = client.db(dbName);
 
   db.dropCollection('campsites', (err, result) => {
-    assert.strictEqual(err, undefined)
+    assert.strictEqual(err, undefined);
+    console.log('Dropped Collection:', result);
 
-    console.log('Dropped Collection', result);
-
-    const collection = db.collection('campsites');
-
-    collection.insertOne(
-      { name: 'Breadcrumb Trail Campground', description: 'Test' },
-      (err, result) => {
-        assert.strictEqual(err, undefined);
-
+    dboper.insertDocument(db, { name: "Breadcrumb Trail Campground", description: "Test" },
+      'campsites', result => {
         console.log('Insert Document:', result.ops);
 
-        collection.find().toArray((err, docs) => {
-          assert.strictEqual(err, undefined);
-
+        dboper.findDocuments(db, 'campsites', docs => {
           console.log('Found Documents:', docs);
 
-          client.close();
+          dboper.updateDocument(db, { name: "Breadcrumb Trail Campground" },
+            { description: "Updated Test Description" }, 'campsites',
+            result => {
+              console.log('Updated Document Count:', result.modifiedCount);
+
+              dboper.findDocuments(db, 'campsites', docs => {
+                console.log('Found Documents:', docs);
+
+                dboper.removeDocument(db, { name: "Breadcrumb Trail Campground" },
+                  'campsites', result => {
+                    console.log('Deleted Document Count:', result.deletedCount);
+
+                    client.close();
+                  }
+                );
+              });
+            }
+          );
         });
-      }
-    );
+      });
   });
 });
